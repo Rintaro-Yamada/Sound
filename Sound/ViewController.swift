4155fcfb8be4e1ed7b3825232e223daf36205cd9
rintaro <alfredrobert18sei@gmail.com>
2021-05-08 15:45:51 +0900

    サウンドアプリ2の完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // ドラムを表示する箱を作る
+    @IBOutlet var drumButton: UIButton!
+    
+    //ドラムのサウンドファイルを読み込んで, プレイヤーをつくる
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    // ドラムのタッチが始まった時に呼ばれる
+    @IBAction func touchDownDrumButton() {
+        
+        // ドラムが鳴っている画像に切り替える
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        
+        //ドラムの音を巻き戻す
+        drumSoundPlayer.currentTime = 0
+        
+        // ドラムの音を再生する
+        drumSoundPlayer.play()
+        
+    }
+    
+    // ドラムボタンの上でタッチが終わる時に呼ばれる
+    @IBAction func touchUpDrumButton() {
+        
+        //ドラムが鳴っていない画像に切り替える
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
 
 
 }
-
